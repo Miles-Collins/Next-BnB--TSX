7ce3274ba9d51b8b0a52b402c99651ddf4a765e6
Miles-Collins <miles.collins@ymail.com>
2023-06-26 16:23:48 -0600

    navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,26 +1,33 @@
+"use client";
+
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
-  return ( 
+  return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-      <Container>
-        <div className="
+        <Container>
+          <div
+            className="
         flex
         flex-row
         items-center
         justify-between
         gap-3
         md:gap-0
-        ">
-          <Logo />
-        </div>
-        Anything
-      </Container>
+        "
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
       </div>
-      </div>
-   );
-}
- 
+    </div>
+  );
+};
+
 export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import './globals.css'
-import { Nunito } from 'next/font/google'
-import { Inter } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import { Inter } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const font = Nunito({
   subsets: ["latin"],
-})
+});
 
 export const metadata = {
-  title: 'Next BnB',
-  description: 'Next BnB, the NEXT Option',
-}
+  title: "Next BnB",
+  description: "Next BnB, the NEXT Option",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
         <Navbar />
-        {children}</body>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
